bbb6efbd3f9d7e0329da459c7d7bcccaf5711b67
OOHehir <40183643+OOHehir@users.noreply.github.com>
2024-02-25 12:52:42 +0000

    Clean-up

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,9 @@
 #include "BluetoothSerial.h"
 #include "driver/gpio.h"
-#include "esp_event_loop.h"
+#include "esp_event.h"
 #include "esp_log.h"
 #include "esp_spi_flash.h"
 #include "esp_system.h"
-#include "esp_wifi.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "nvs_flash.h"
@@ -15,90 +14,70 @@
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
-#define LED_BT_BLUE                                                            \
-  2 // BT: Internal LED (or LED on the pin D2) for the connection indication
-    // (connected LED ON / disconnected LED OFF)
-#define LED_BT_RED                                                             \
-  15 // BT: LED (LED on the pin D4) for the connection indication (connected LED
-     // OFF / disconnected LED ON)
-#define LED_PIN GPIO_NUM_8             // Heartbeat
-unsigned long previousMillisReconnect; // BT: Variable used for comparing millis
-                                       // counter for the reconnection timer
-bool ledBtState = false; // BT: Variable used to change the indication LED state
-bool SlaveConnected; // BT: Variable used to store the current connection state
-                     // (true=connected/false=disconnected)
-int recatt = 0;      // BT: Variable used to count the reconnection attempts
+// Flashing-> not connected, steady -> connected
+#define BT_CONNECTION_LED GPIO_NUM_2
+// Flashing-> normal
+#define HEARTBEAT_LED GPIO_NUM_8
 
-String myName =
-    "ESP32-BT-Master"; // BT: Variable used to store the SERVER(Master)
-                       // bluetooth device name; just for prinitng
-String slaveName =
-    "ELOC_NONAME"; // BT: Variable used to store the CLIENT(Slave) bluetooth
-                      // device name; just for prinitng; just for printing in
-                      // this case
-String MACadd =
-    "03:B4:16:72:36:9C"; // BT: Variable used to store the CLIENT(Slave)
-                         // bluetooth device Mac address; just for prinitng;
-                         // just for printing in this case
-uint8_t address[6] = {0xC8, 0xF0, 0x9E, 0xFB, 0xBE, 0x4E}; // BT: Variable used to store the CLIENT(Slave) MAC address
-                 // used for the connection; Use your own andress in the same
-                 // format
+int reconnection_count = 0;
+bool bt_connected = false;
 
+String relay_bt_name = "ESP32-BT-Master";
+char identifier[] = "[Relay] ";
+uint8_t slave_mac_address[6] = {0xC8, 0xF0, 0x9E, 0xFB, 0xBE, 0x4E};
 BluetoothSerial SerialBT;
-
-void flashLED(void *) {
-  while (1) {
-    gpio_set_level(LED_PIN, 1);
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-    gpio_set_level(LED_PIN, 0);
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-  }
-}
-
-// BT: Bt_Status callback function
-void Bt_Status(esp_spp_cb_event_t event, esp_spp_cb_param_t *param) {
-  if (event ==
-      ESP_SPP_OPEN_EVT) { // BT: Checks if the SPP connection is open, the event
-                          // comes// event == Client connected
-    Serial.println("Client Connected"); // BT: Write to the serial monitor
-    digitalWrite(
-        LED_BT_BLUE,
-        HIGH); // BT: Turn ON the BLUE bluetooth indication LED (solid light)
-    digitalWrite(LED_BT_RED,
-                 LOW); // BT: Turn OFF the RED bluetooth indication LED
-    SlaveConnected =
-        true;   // BT: Set the variable true = CLIENT is connected to the SERVER
-    recatt = 0; // BT: Reset the reconnect attempts counter
-  } else if (event == ESP_SPP_CLOSE_EVT) { // BT: event == Client disconnected
-    Serial.println("Client Disconnected"); // BT: Write to the serial monitor
-    digitalWrite(
-        LED_BT_RED,
-        HIGH); // BT: Turn ON the RED bluetooth indication LED (solid light)
-    digitalWrite(LED_BT_BLUE,
-                 LOW); // BT: Turn OFF the BLUE bluetooth indication LED
-    SlaveConnected =
-        false; // BT: Set the variable false = CLIENT connection lost
-  }
-}
-
-void SlaveConnect() { // BT: This function connects/reconnects to the
-                      // CLIENT(Slave)
-  Serial.println(
-      "Function BT connection executed"); // BT: Write to the serial monitor
-  Serial.printf("Connecting to slave BT device named \"%s\" and MAC address "
-                "\"%s\" is started.\n",
-                slaveName.c_str(),
-                MACadd.c_str()); // BT: Write to the serial monitor
-  SerialBT.connect(
-      address); // BT: Establishing the connection with the CLIENT(Slave) with
-                // the Mac address stored in the address variable
-}
 
 extern "C" {
 void app_main(void);
 }
 
+void flashLED() {
+  while (1) {
+    gpio_set_level(HEARTBEAT_LED, HIGH);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    gpio_set_level(HEARTBEAT_LED, LOW);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+    if (bt_connected) {
+      gpio_set_level(BT_CONNECTION_LED, HIGH);
+    } else {
+      gpio_set_level(BT_CONNECTION_LED, ~gpio_get_level(BT_CONNECTION_LED));
+    }
+  }
+}
+
+void bt_status_callback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param) {
+  if (event == ESP_SPP_OPEN_EVT) {
+    Serial.println("Client Connected");
+    bt_connected = true;
+    reconnection_count = 0;
+  } else if (event == ESP_SPP_CLOSE_EVT) {
+    Serial.println("Client Disconnected");
+    bt_connected = false;
+  }
+}
+
+void slave_connect() {
+  static u_int32_t previousMillisReconnect = 0;
+  // Wait 1 sec between attempts
+  if (millis() - previousMillisReconnect >= 10000) {
+    Serial.printf("This device BT name: %s\n", relay_bt_name.c_str());
+    Serial.printf("Attempting to connect to device address: ");
+    for (auto i = 0; i < sizeof(slave_mac_address); i++) {
+      // TODO: Serial.printf("0x%c, ", (const char*)(slave_mac_address[i]));
+    }
+    Serial.println();
+    previousMillisReconnect = millis();
+    Serial.print("Attempt No.: ");
+    Serial.println(++reconnection_count);
+    SerialBT.end();
+    SerialBT.begin(relay_bt_name, true);
+    SerialBT.connect(slave_mac_address);
+  }
+}
+
 void app_main(void) {
+  // TODO: start LED task
   Serial.begin(115200);
   delay(3000);
   printf("\n");
@@ -129,54 +108,22 @@ void app_main(void) {
   }
   ESP_ERROR_CHECK(ret);
 
-  SerialBT.register_callback(Bt_Status);
-  SerialBT.begin(myName, true);  // Bluetooth device name, start as master
-  SlaveConnect();
+  SerialBT.register_callback(bt_status_callback);
+  SerialBT.begin(relay_bt_name, true);
 
   while (1) {
+    if (!bt_connected) {
+        slave_connect();
+    }
+
     if (Serial.available()) {
       SerialBT.write(Serial.read());
     }
-    if (SerialBT.available()) {
-      Serial.write(SerialBT.read());
-    }
 
-    if (!SlaveConnected) { // BT: Condition to evalute if the connection is
-                           // established/lost
-      if (millis() - previousMillisReconnect >=
-          10000) { // BT: Check that 10000ms is passed
-        previousMillisReconnect =
-            millis(); // BT: Set previousMillisReconnect to current millis
-        recatt++;     // BT: Increase the the reconnection attempts counter +1
-        Serial.print("Trying to reconnect. Attempt No.: "); // BT: Write to the
-                                                            // serial monitor
-        Serial.println(
-            recatt); // BT: Write the attempts count to the serial monitor
-        Serial.println(
-            "Stopping Bluetooth..."); // BT: Write to the serial monitor
-        SerialBT.end();               // BT: Close the bluetooth device
-        Serial.println(
-            "Bluetooth stopped !"); // BT: Write to the serial monitor
-        Serial.println(
-            "Starting Bluetooth..."); // BT: Write to the serial monitor
-        SerialBT.begin(myName,
-                       true); // BT: Starts the bluetooth device with the name
-                              // stored in the myName variable as SERVER(Master)
-        Serial.printf("The device \"%s\" started in master mode, make sure "
-                      "slave BT device is on!\n",
-                      myName.c_str());
-        SlaveConnect(); // BT: Calls the bluetooth connection function to cnnect
-                        // to the CLIENT(Slave)
-      }
-    }
-    // BT: Data send/receive via bluetooth
-    if (Serial.available()) { // BT: Checks if there are data from the serial
-                              // monitor available
-      SerialBT.write(Serial.read()); // BT: Sends the data via bluetooth
-    }
-    if (SerialBT.available()) {      // BT: Checks if there are data from the
-                                     // bluetooth available
-      Serial.write(SerialBT.read()); // BT: Write the data to the serial monitor
+    if (SerialBT.available()) {
+      // Add an identifier at the start of the msg
+      String s = identifier + SerialBT.read();
+      Serial.write(s.c_str());
     }
 
     vTaskDelay(10 / portTICK_PERIOD_MS);
